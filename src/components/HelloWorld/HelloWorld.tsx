c7dab4dc6dbd88c2ff1e646abe62145e0daf2777
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-05 17:11:12 +0000

    add test component and utility file - test project path aliases and testing libraries work

---
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const HelloWorld: FC = () => (
+    <>
+        <p>Hello World</p>
+    </>
+);
+
+export default HelloWorld;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import HelloWorld from '@components/HelloWorld/HelloWorld';
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <HelloWorld />
+      </main>
 
     </div>
   )
