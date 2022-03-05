7f793027951a7133f247d11b221703966387f9bd
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-05 17:03:34 +0000

    add custom jest configuration: setup moduleNameMapper to match project directory aliases in jest.config.js. setup jest.setup.js file and include extra library: extend-expect. add jest npm script

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,12 @@ const customJestConfig = {
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+        '^@src/(.*)$': '<rootDir>/src/$1',
+        '^@components/(.*)$': '<rootDir>/src/components/$1',
+        '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
