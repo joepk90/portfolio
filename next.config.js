155be35618f7c6597b6ca883878bac68cfae9e80
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-06 21:35:28 +0000

    setup global css config in next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { withGlobalCss } = require('next-global-css')
+const withConfig = withGlobalCss()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withConfig(nextConfig)
