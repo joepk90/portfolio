47e918c9769c8351878e62153dfef9605f69176e
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-06 19:54:05 +0000

    setup default seo configuration in _app.tsx file and update the root index page. add a seo-congif.ts file. update the layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo';
+import SEO from '@src/seo-config';
 import 'normalize.css';
 import '@styles/style.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ src/seo-config.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'Joe Park-Kennaby: Personal Portfolio',
+    author: {
+        name: 'Joe Park-Kennaby',
+        summary: 'Software Engineer',
+    },
+    description: 'A NextJS website to showcase my personal projects, work experience and anything else of relevance.',
+    canonical: 'http://localhost:3000',
+}
