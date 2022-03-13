14e0cc79fc7582faf33e490e9f311bb3736ac64f
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-13 11:10:33 +0000

    setup storybook scss rule .storybook/main.js file (storybook-addon-sass-postcss is required)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -11,5 +13,21 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "webpack5"
-  }
+  },
+
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // storybook-addon-sass-postcss required as a dependancy
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader?url=false', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 }
