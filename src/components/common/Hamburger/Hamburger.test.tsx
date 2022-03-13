4203d86444c34c6ee20cb3cfd5fa5d4d5ce878ee
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-13 15:18:15 +0000

    setup Hamburger component (component, tests and stories)

---
+++ src/components/common/Hamburger/Hamburger.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Hamburger from '@components/common/Hamburger/Hamburger';
+
+const mockedOnClick = jest.fn();
+
+const mockedHamburgerElement = () => {
+    return (
+        <Hamburger onClick={mockedOnClick}></Hamburger>
+    )
+}
+
+const getHamburgerElement = () => {
+    render(mockedHamburgerElement());
+    return screen.getByTestId('hamburger')
+}
+
+describe("Hamburger", () => {
+
+    it('should render the hamburger outer element', () => {
+        const hamburgerElement = getHamburgerElement();
+        expect(hamburgerElement).toBeInTheDocument();
+    });
+
+    it('should render the hamburger-box element', () => {
+        const hamburgerElement = getHamburgerElement();
+        expect(hamburgerElement.getElementsByClassName('hamburger-box').length).toBe(1)
+    });
+
+    it('should render the hamburger-inner element', () => {
+        const hamburgerElement = getHamburgerElement();
+        expect(hamburgerElement.getElementsByClassName('hamburger-inner').length).toBe(1)
+    });
+
+    it('should render is-active class when clicked once', () => {
+        const hamburgerElement = getHamburgerElement();
+        fireEvent.click(hamburgerElement)
+        expect(hamburgerElement.getAttribute('class')).toMatch(/is-active/gi);
+    });
+
+    it('should not render is-active class when clicked twice', () => {
+        const hamburgerElement = getHamburgerElement();
+        fireEvent.click(hamburgerElement);
+        fireEvent.click(hamburgerElement);
+        expect(hamburgerElement.getAttribute('class')).not.toMatch(/is-active/gi);
+    });
+
+});
---
+++ src/components/common/Hamburger/Hamburger.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import "@components/common/Hamburger/Hamburger.scss";
+
+
+type Props = {
+    onClick: Function
+}
+
+const Hamburger = ({ onClick }: Props) => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const renderClassList = (): string => {
+
+        let classList = "hamburger hamburger--spin";
+
+        if (isActive === true) {
+            classList = classList + " is-active";
+        }
+
+        return classList;
+    }
+
+    const handleClick = (): void => {
+
+        setIsActive(!isActive);
+
+        onClick();
+
+    }
+
+    return (
+        <button
+            className={renderClassList()}
+            type="button"
+            onClick={() => handleClick()}
+            data-testid="hamburger"
+        >
+            <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
+            </span>
+        </button>
+    );
+}
+
+export default Hamburger;
