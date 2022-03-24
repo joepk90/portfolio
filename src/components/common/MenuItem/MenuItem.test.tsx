2ae856ad363af71cd51b8eacac43223fe430c7cc
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-24 16:20:16 +0000

    setup MenuItem component with stories configuration and component tests

---
+++ src/components/common/MenuItem/MenuItem.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MenuItem from '@components/common/MenuItem/MenuItem';
+
+const path = '/';
+const label = 'Menu Item';
+
+const mockedMenuItemElement = () => {
+    return (
+        <MenuItem
+            path={path}
+            label={label}
+            id={path}
+        ></MenuItem>
+    )
+}
+
+const getMockedElement = () => {
+    render(mockedMenuItemElement());
+    return screen.getByRole('menuitem')
+}
+
+describe("MenuItem", () => {
+
+    it('should render the wrapper element', () => {
+        const element = getMockedElement();
+        expect(element).toBeInTheDocument();
+    });
+
+    it('should render the provided text', () => {
+        const element = getMockedElement();
+        expect(element.textContent).toContain(label);
+    });
+
+    it('should render a link (a) element with an href of the provided path', () => {
+        const element = getMockedElement();
+        expect(element.getAttribute('href')).toBe(path);
+    });
+});
---
+++ src/components/common/MenuItem/MenuItem.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link from 'next/link';
+import { generateBEMModifiersClassList } from '@utilities/utilities';
+import '@components/common/MenuItem/MenuItem.scss';
+
+type Variant = 'dropdown';
+
+export type Props = {
+    id: string,
+    label: string,
+    path: string
+    variant?: Variant
+}
+
+const MenuItem = ({ path, label, variant }: Props) => {
+
+    const classModifiers = variant ? [variant] : [];
+
+    const classList = generateBEMModifiersClassList('menu-item', classModifiers)
+
+    return (
+        <li className={classList}>
+            <Link href={path}>
+                <a className='menu-item__link' role="menuitem">{label}</a>
+            </Link>
+        </li>
+    )
+}
+
+export default MenuItem;
