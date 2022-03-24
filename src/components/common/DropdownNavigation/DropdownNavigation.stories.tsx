688e2b32f926d9965544fd72891ce7e08a7d907e
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-24 16:30:38 +0000

    setup DropdownNavigation component with stories configuration and component tests

---
+++ src/components/common/DropdownNavigation/DropdownNavigation.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Story, ComponentMeta } from '@storybook/react';
+import DropdownNavigation, { Props } from '@src/components/common/DropdownNavigation/DropdownNavigation'; // TODO setup aliases in storybook..
+import { menuItems } from '@src/config'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Molecules/DropdownNavigation',
+    component: DropdownNavigation,
+    argTypes: {
+        isOpen: {
+            options: [true, false],
+            control: { type: 'boolean' },
+        },
+    },
+} as ComponentMeta<typeof DropdownNavigation>;
+
+const Template: Story<Props> = (args: any) => {
+    return (
+        <DropdownNavigation {...args} menuItems={menuItems} variant='dropdown'></DropdownNavigation>
+    );
+};
+
+export const Primary = Template.bind({});
+Primary.storyName = 'DropdownNavigation';
+Primary.args = {
+    isOpen: true
+}
---
+++ src/components/common/DropdownNavigation/DropdownNavigation.test.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import DropdownNavigation from '@src/components/common/DropdownNavigation/DropdownNavigation';
+
+const menuItems = [
+    {
+        id: '1',
+        path: '1',
+        label: '1'
+    },
+    {
+        id: '2',
+        path: '2',
+        label: '2'
+    }
+]
+
+const renderMockedElement = (isOpen: boolean = false) => {
+    return (
+        render(<DropdownNavigation menuItems={menuItems} isOpen={isOpen} />)
+    )
+}
+
+describe("DropdownNavigation", () => {
+
+    it('should be in the document, but set to hidden (aria-hidden)', () => {
+        renderMockedElement()
+        expect(screen.getByRole('navigation', { hidden: true })).toBeInTheDocument();
+    });
+
+    it('navigation element should not have the class is-open', () => {
+        renderMockedElement()
+        const nav = screen.getByRole('navigation', { hidden: true });
+        expect(nav).not.toHaveClass('is-open');
+    });
+
+    it('navigation element should be in the document', () => {
+        renderMockedElement(true);
+        expect(screen.getByRole('navigation')).toBeInTheDocument();
+    });
+
+    it('navigation element should have the class is-open', () => {
+        renderMockedElement(true)
+        const nav = screen.getByRole('navigation');
+        expect(nav).toHaveClass('is-open');
+    });
+
+});
+
+describe("DropdownNavigation MenuItems", () => {
+
+    it('should render multiple (2) menu items', () => {
+        renderMockedElement(true);
+        expect(screen.getAllByRole('menuitem').length).toBe(2);
+    });
+
+    it('should render the first menu item text content', () => {
+        renderMockedElement(true);
+        let link = screen.getAllByRole('menuitem')[0] as HTMLAnchorElement;
+        expect(link.textContent).toContain('1');
+    });
+
+    it('should render the first menu item href attribute', () => {
+        renderMockedElement(true);
+        let link = screen.getAllByRole('menuitem')[0] as HTMLAnchorElement;
+        expect(link.href).toContain('/1');
+    });
+
+    it('should render the second menu item text content', () => {
+        renderMockedElement(true);
+        let link = screen.getAllByRole('menuitem')[1] as HTMLAnchorElement;
+        expect(link.textContent).toContain('2');
+    });
+
+    it('should render the second menu item href attribute', () => {
+        renderMockedElement(true);
+        let link = screen.getAllByRole('menuitem')[1] as HTMLAnchorElement;
+        expect(link.href).toContain('/2');
+    });
+
+});
