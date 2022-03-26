3ed44a7759800a3173e1df74813154f697fe14af
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-26 16:42:14 +0000

    update Footer component - add title, site description, navigation and copyright elements. setup simple test suite

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { menuItems, siteDescription } from '@src/config';
 import Footer from '@components/Footer/Footer';
 
 const renderMockedElement = () => {
@@ -9,13 +10,23 @@ const renderMockedElement = () => {
 
 describe('footer component', () => {
 
-    it('should render the site logo', () => {
+    it('should render the site title', () => {
         renderMockedElement();
-        expect(screen.getByTestId('site-logo')).toBeInTheDocument();
+        expect(screen.getByTestId('site-title')).toBeInTheDocument();
     });
 
     it('should render a navigation component', () => {
         renderMockedElement();
         expect(screen.getAllByRole('navigation').length).toBe(1);
+    });
+
+    it('should render the site description', () => {
+        renderMockedElement();
+        expect(screen.getByText(siteDescription)).toBeInTheDocument();
+    });
+
+    it('should render the footer copyright element', () => {
+        renderMockedElement();
+        expect(screen.getByTestId('footer-copyright')).toBeInTheDocument();
     });
 });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,38 @@
 import Navigation from '@src/components/common/Navigation/Navigation'
-import SiteLogo from '@src/components/SiteLogo/SiteLogo';
-import { menuItems } from '@src/config';
+import Section from '@src/components/common/Section/Section'
+import Container from '@src/components/common/Container/Container'
+import SiteTitle from '@components/SiteTitle/SiteTitle';
+import { menuItems, siteDescription } from '@src/config';
 import '@components/Footer/Footer.scss'
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer__container">
-                <div className="footer__section">
-                    <Navigation menuItems={menuItems} />
-                    <SiteLogo />
-                </div>
-            </div>
+
+            <Section margin='lg'>
+                <Container>
+                    <div className="footer__row">
+                        <div className="footer__column">
+                            <p><SiteTitle variant='light' /></p>
+                            <p className="footer__content">{siteDescription}</p>
+                        </div>
+
+                        <div className="footer__column">
+                            <h4 className='footer__title'>Site Map</h4>
+                            <div className="footer__content">
+                                <Navigation menuItems={menuItems} variant='light' />
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </Section>
+
+            <Section marginBottom='sm'>
+                <Container>
+                    <div className="footer__copyright" data-testid='footer-copyright'>@ Joe Park-Kennaby 2022</div>
+                </Container>
+            </Section>
+
         </footer>
     )
 }
