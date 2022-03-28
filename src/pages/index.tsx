706424e6a6c501f3d80def0fe03dbf672f5b2def
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-28 20:00:31 +0100

    setup index.tsx template using contentful data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,42 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
-import Layout from '@src/components/Layout/Layout'
+import { getIndexStaticProps, IndexProps } from '@src/lib/hooks/index';
+import ReactMarkdown from 'react-markdown';
+import { ContentfulImageAlias as ContentfulImage } from '@components/common/ContentfulImage/ContentfulImage';
+import Section from '@components/common/Section/Section';
+import Heading from '@components/common/Heading/Heading';
+import Container from '@src/components/common/Container/Container';
+import Layout from '@components/Layout/Layout';
 import { SEO } from '@src/config';
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => getIndexStaticProps();
+
+const Index: NextPage<IndexProps> = ({ pageContent, profileImage, profileBio }) => {
+
   return (
     <Layout>
       <NextSeo
         title={`${SEO.title} | Home`}
       />
 
-      <p>Home</p>
+      <Section margin='lg'>
+        <Container>
+          <ContentfulImage image={profileImage} />
+          <Heading>Hi There!</Heading>
+          <ReactMarkdown>{profileBio}</ReactMarkdown>
+        </Container>
+      </Section>
+
+
+      <Section margin='lg'>
+        <Container>
+          <ReactMarkdown>{pageContent}</ReactMarkdown>
+          {/* <Tags/> */}
+        </Container>
+      </Section>
 
     </Layout>
   )
 }
 
-export default Home
+export default Index
