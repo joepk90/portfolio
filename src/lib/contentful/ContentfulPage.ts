e7b3702a3f44e6a2c39674cf53676347b4714efa
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-28 19:39:20 +0100

    setup contentful lib child class: ContentfulPage + tests

---
+++ src/lib/contentful/ContentfulPage.ts
@@ -0,0 +1,28 @@
+import { ContentfulEntryManager } from '@src/lib/contentful/ContentfulEntryManager';
+
+export interface ContentfulPageInterface {
+    getId(): string
+    getTitle(): string
+    getContent(): string
+}
+
+export type Page = {
+    id: string
+    title: string
+    content: string
+}
+
+export class ContentfulPage extends ContentfulEntryManager<Page> implements ContentfulPageInterface {
+
+    getId = (): string => {
+        return this.getField('id')
+    }
+
+    getTitle = (): string => {
+        return this.getField('title')
+    }
+
+    getContent = (): string => {
+        return this.getField('content')
+    }
+}
