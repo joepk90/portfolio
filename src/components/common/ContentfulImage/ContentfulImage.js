7b60137e5b8854f81d88a2c3cdba283e3e4837b1
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-28 19:54:45 +0100

    setup ContentfulImage component using @moxy/react-contentful-image package

---
+++ src/components/common/ContentfulImage/ContentfulImage.js
@@ -0,0 +1,6 @@
+import ContentfulImage from '@moxy/react-contentful-image';
+
+// TODO Setup as default export called ContentfulImage
+export const ContentfulImageAlias = ({ image }) => {
+    return <ContentfulImage image={image} />
+}
