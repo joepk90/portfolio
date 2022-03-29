61a252163639aeb77b5b209f87b293757976e697
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-29 21:18:42 +0100

    update the index layout - use the FlexWrapper and Grid, Row, Col components to handle layout styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
-import { getIndexStaticProps, IndexProps } from '@src/lib/hooks/index';
 import ReactMarkdown from 'react-markdown';
+import { GridContainer, Row, Col } from '@components/common/Grid/Grid';
+import { getIndexStaticProps, IndexProps } from '@src/lib/hooks/index';
 import { ContentfulImageAlias as ContentfulImage } from '@components/common/ContentfulImage/ContentfulImage';
 import Section from '@components/common/Section/Section';
 import Heading from '@components/common/Heading/Heading';
 import Container from '@src/components/common/Container/Container';
 import Layout from '@components/Layout/Layout';
+import FlexWrapper from '@components/common/FlexWrapper/FlexWrapper';
+import BrushStrokeWrapper from '@components/common/BrushStrokeWrapper/BrushStrokeWrapper';
 import { SEO } from '@src/config';
+
 
 export const getStaticProps = async () => getIndexStaticProps();
 
@@ -21,17 +25,44 @@ const Index: NextPage<IndexProps> = ({ pageContent, profileImage, profileBio }) 
 
       <Section margin='lg'>
         <Container>
-          <ContentfulImage image={profileImage} />
-          <Heading>Hi There!</Heading>
-          <ReactMarkdown>{profileBio}</ReactMarkdown>
+          <GridContainer>
+            <Row>
+
+              <Col sm={12} md={6} xl={5}>
+                <FlexWrapper justifyContent='center'>
+                  <FlexWrapper maxWidth={325} >
+                    <BrushStrokeWrapper>
+                      <ContentfulImage image={profileImage} />
+                    </BrushStrokeWrapper>
+                  </FlexWrapper>
+                </FlexWrapper>
+              </Col>
+
+              <Col sm={12} md={6} xl={5}>
+                <FlexWrapper flexDirection='column' justifyContent='center' height='100%'>
+                  <Heading style={{ fontSize: '54px', marginBottom: '0.2em' }}>Hi there!</Heading>
+                  <ReactMarkdown className="h5-font-size">{profileBio}</ReactMarkdown>
+                </FlexWrapper>
+              </Col>
+
+            </Row>
+          </GridContainer>
         </Container>
       </Section>
 
-
       <Section margin='lg'>
         <Container>
-          <ReactMarkdown>{pageContent}</ReactMarkdown>
-          {/* <Tags/> */}
+          <GridContainer>
+            <Row>
+
+              <Col sm={12} md={7}>
+                <ReactMarkdown>{pageContent}</ReactMarkdown>
+              </Col>
+              <Col sm={12} md={5}>
+                {/* <Tags/> */}
+              </Col>
+
+            </Row></GridContainer>
         </Container>
       </Section>
 
