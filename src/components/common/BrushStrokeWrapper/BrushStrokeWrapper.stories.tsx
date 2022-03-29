c2246130da0ece06b3752aaf5ed4a45fb4ef1701
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-29 19:11:54 +0100

    setup BrushStrokeWrapper with tests and stories

---
+++ src/components/common/BrushStrokeWrapper/BrushStrokeWrapper.stories.tsx
@@ -0,0 +1,44 @@
+import { Story, ComponentMeta } from '@storybook/react';
+import { BrushStrokeSVGClipPath } from '@components/common/BrushStroke/BrushStroke'
+// import Image from 'next/image';
+import { ContentfulImageAlias as ContentfulImage } from '@components/common/ContentfulImage/ContentfulImage';
+
+import BrushStrokeWrapper, { BrushStrokeWrapperProps } from '@components/common/BrushStrokeWrapper/BrushStrokeWrapper';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Molecules/BrushStrokeWrapper',
+    component: BrushStrokeWrapper,
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            },
+        }
+    }
+} as ComponentMeta<typeof BrushStrokeWrapper>;
+
+const Template: Story<BrushStrokeWrapperProps> = (args: BrushStrokeWrapperProps) => {
+    return (
+        <>
+            <BrushStrokeSVGClipPath />
+            <div style={{ maxWidth: '300px' }}>
+
+                <BrushStrokeWrapper {...args} />
+            </div>
+        </>
+    )
+}
+
+// const image = () => 
+export const ImageDecoration = Template.bind({});
+ImageDecoration.args = {
+    children: <ContentfulImage image='http://via.placeholder.com/300.png' /> // TODO replace with nextjs Image
+}
+
+export const ElementDecoration = Template.bind({});
+ElementDecoration.args = {
+    children: <div style={{ width: '300px', height: '300px', background: 'red' }} />
+}
+
+
---
+++ src/components/common/BrushStrokeWrapper/BrushStrokeWrapper.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { BrushStrokeSVGClipPath } from '@components/common/BrushStroke/BrushStroke';
+import BrushStrokeWrapper from '@components/common/BrushStrokeWrapper/BrushStrokeWrapper';
+
+const mockedElement = () => {
+    return (
+        <>
+            <BrushStrokeSVGClipPath />
+            <BrushStrokeWrapper>
+                <div data-testid="brush-stroke-wrapper-child" style={{ width: '300px', height: '300px', background: 'red' }} />
+            </BrushStrokeWrapper>
+        </>
+    )
+}
+
+describe("BrushStrokeWrapper component", () => {
+
+    it('should render the element used for decoration', () => {
+        render(mockedElement())
+        expect(screen.queryAllByRole('presentation').length).toBe(2);
+    });
+
+    it('should render the child element passed in', () => {
+        render(mockedElement())
+        expect(screen.getByTestId('brush-stroke-wrapper-child')).toBeInTheDocument();
+    });
+});
