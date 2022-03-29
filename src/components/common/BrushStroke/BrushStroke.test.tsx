39148be891992ce27dca9489a472603c663ee725
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-29 18:44:20 +0100

    setup BrushStroke component + tests and story

---
+++ src/components/common/BrushStroke/BrushStroke.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import BrushStroke, { BrushStrokeSVGClipPath } from '@components/common/BrushStroke/BrushStroke';
+
+const mockedElement = () => {
+    return (
+        <>
+            <BrushStrokeSVGClipPath />
+            <BrushStroke />
+        </>
+    )
+}
+
+describe("BrushStroke component", () => {
+
+    it('should render the element used for decoration', () => {
+        render(mockedElement())
+        expect(screen.getByRole('presentation')).toBeInTheDocument();
+    });
+
+    it('should the clippath element', () => {
+        render(mockedElement())
+        expect(screen.getByTestId('brush-stroke-clip-path')).toBeInTheDocument();
+    });
+});
