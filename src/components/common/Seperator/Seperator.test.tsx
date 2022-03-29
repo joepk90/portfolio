ab2a1edd76695ba5a84dff5cb3e4e195ad374da0
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-29 17:48:25 +0100

    setup new Seperator component + story and tests

---
+++ src/components/common/Seperator/Seperator.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Seperator from '@components/common/Seperator/Seperator';
+
+const renderMockedElement = () => {
+    return (
+        render(<Seperator />)
+    )
+}
+
+describe('seperator component', () => {
+
+    it('should render a component with class name of seperator', () => {
+        const { container } = renderMockedElement();
+        expect(container.getElementsByClassName('seperator').length).toBe(1);
+    });
+});
---
+++ src/components/common/Seperator/Seperator.tsx
@@ -0,0 +1,11 @@
+import "@components/common/Seperator/Seperator.scss";
+
+export type Props = {}
+
+const Seperator = (props: Props) => {
+    return (
+        <div className="seperator"></div>
+    )
+}
+
+export default Seperator;
