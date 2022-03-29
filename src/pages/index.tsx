c276f9a7c47fab77d4f0e13a2a25f308ae7a0c00
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-29 23:33:03 +0100

    setup Tags component in index pagae

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,13 @@ import { GridContainer, Row, Col } from '@components/common/Grid/Grid';
 import { getIndexStaticProps, IndexProps } from '@src/lib/hooks/index';
 import { ContentfulImageAlias as ContentfulImage } from '@components/common/ContentfulImage/ContentfulImage';
 import Section from '@components/common/Section/Section';
+import Tags from '@components/common/Tags/Tags';
 import Heading from '@components/common/Heading/Heading';
 import Container from '@src/components/common/Container/Container';
 import Layout from '@components/Layout/Layout';
 import FlexWrapper from '@components/common/FlexWrapper/FlexWrapper';
 import BrushStrokeWrapper from '@components/common/BrushStrokeWrapper/BrushStrokeWrapper';
-import { SEO } from '@src/config';
-
+import { SEO, tagsList } from '@src/config';
 
 export const getStaticProps = async () => getIndexStaticProps();
 
@@ -55,11 +55,13 @@ const Index: NextPage<IndexProps> = ({ pageContent, profileImage, profileBio }) 
           <GridContainer>
             <Row>
 
-              <Col sm={12} md={7}>
+              <Col sm={12} md={6} lg={6}>
                 <ReactMarkdown>{pageContent}</ReactMarkdown>
               </Col>
-              <Col sm={12} md={5}>
-                {/* <Tags/> */}
+              <Col sm={12} md={6} lg={6}>
+                <FlexWrapper flexDirection='column' alignItems='flex-end' justifyContent='center' height='100%'>
+                  <Tags tags={tagsList} style={{ justifyContent: 'center' }} />
+                </FlexWrapper>
               </Col>
 
             </Row></GridContainer>
