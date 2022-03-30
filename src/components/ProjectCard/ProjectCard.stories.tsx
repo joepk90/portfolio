bd0367cc32055181578294ae6a59593fa645c3bf
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-30 22:47:24 +0100

    setup ProjectCard component with storybook export and tests

---
+++ src/components/ProjectCard/ProjectCard.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentMeta, Story } from '@storybook/react';
+import ProjectCard, { ProjectCardComponentProps } from '@components/ProjectCard/ProjectCard';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Organisms/ProjectCard',
+    component: ProjectCard,
+} as ComponentMeta<typeof ProjectCard>;
+
+
+const Template: Story<ProjectCardComponentProps> = (args: ProjectCardComponentProps) => {
+    return (
+        <ProjectCard {...args}></ProjectCard>
+    );
+};
+
+export const Default = Template.bind({});
+Default.storyName = 'ProjectCard';
+Default.args = {
+    project: {
+        date: 'March 2022',
+        description: 'The Description!',
+        repositories: ['https://www.example.com'],
+        slug: 'project-slug',
+        title: 'The Project Title',
+        type: 'Personal',
+        url: 'https://www.example.com',
+        tags: ['Typescript', 'React'],
+    }
+}
---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,139 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { ProjectProps } from '@src/lib/contentful/ContentfulProject';
+import Tags from '@components/common/Tags/Tags';
+import '@components/ProjectCard/ProjectCard.scss';
+
+export type ProjectCardComponentProps = {
+    project: ProjectProps
+}
+
+const Project = ({ project }: ProjectCardComponentProps) => {
+
+    const {
+        date,
+        description,
+        repositories,
+        slug,
+        title,
+        type,
+        url,
+        tags
+    } = project;
+
+    const renderDate = () => {
+
+        if (!date) return '';
+
+        return (
+            <div className="project-card__date">
+                {date}
+            </div>
+        )
+    }
+
+    const renderRepositories = () => {
+
+        if (!repositories || repositories.length === 0) return '';
+
+        return (
+            <div className="project-card__repositories">
+                <strong>Repositories:</strong>
+                <ul className="project-card__repository">
+                    {repositories.map((repository, key) => {
+                        return (
+                            <li key={key}>
+                                <a href={repository}>{repository}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+
+    const renderSlug = () => {
+
+        if (!slug) return '';
+
+        return (
+            <a className="project-card__slug" title={title} href={slug}>
+                {slug}
+            </a>
+        )
+    }
+
+    const renderTitle = () => {
+
+        if (!title) return '';
+
+        return (
+            <h2 className="project-card__title">
+                {title}
+            </h2>
+        )
+    }
+
+    const renderType = () => {
+
+        if (!type) return '';
+
+        return (
+            <span className="project-card__type">
+                <strong>Project Type:</strong> {type}
+            </span>
+        )
+    }
+
+    const renderUrl = () => {
+
+        if (!url) return '';
+
+        return (
+            <div className="project-card__url">
+                <strong>Project URL:</strong> <a href={url}>{url}</a>
+            </div>
+        )
+    }
+
+    const renderDescription = () => {
+
+        if (!description || typeof description === 'string') return '';
+
+        return (
+            <div className="project-card__description">
+                <strong>Project Description: </strong>
+                {documentToReactComponents(description)}
+            </div>
+        )
+    }
+
+    const renderTags = () => {
+
+        if (!tags || tags.length === 0) return '';
+
+        return (
+            <div className="project-card__technologies">
+                <strong>Technologies:</strong>
+                <div className="project-card__tags">
+                    <Tags tags={tags} />
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className='project-card'>
+            <div className="project-card__heading">
+                {renderTitle()}
+                {renderDate()}
+            </div>
+            {renderUrl()}
+            {renderType()}
+            {renderTags()}
+            {renderRepositories()}
+            {renderDescription()}
+        </div>
+    )
+}
+
+export default Project
