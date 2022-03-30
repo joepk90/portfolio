a40cbfe26705e1664a9bf05982469e91f2d9a7f5
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-30 23:07:52 +0100

    setup projects page to render a list of projects using the ProjectCard componet

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,6 +5,7 @@ import Section from '@components/common/Section/Section';
 import Container from '@src/components/common/Container/Container';
 import Layout from '@components/Layout/Layout';
 import Heading from '@components/common/Heading/Heading';
+import Project from '@src/components/ProjectCard/ProjectCard'
 import { SEO } from '@src/config';
 
 export const getStaticProps = async () => getProjectsStaticProps();
@@ -19,7 +20,16 @@ const Index: NextPage<ProjectsProps> = ({ projects }) => {
 
             <Section margin='lg'>
                 <Container>
-                    <Heading>Projects</Heading>
+                    <Heading className='page-title'>Projects</Heading>
+                </Container>
+            </Section>
+
+            <Section margin='lg' paddingBottom='md'>
+                <Container>
+                    {projects.map((project, key) => {
+                        return <Project key={key} project={project} />
+                    })}
+
                 </Container>
             </Section>
 
