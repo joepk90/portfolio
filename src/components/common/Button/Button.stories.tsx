37ffa748e4fb8fdc7e577bf9a999578322139315
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-31 16:45:21 +0100

    setup Button component story

---
+++ src/components/common/Button/Button.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Story, ComponentMeta } from '@storybook/react';
+import Button, { ButtonProps } from '@src/components/common/Button/Button'; // TODO setup aliases in storybook..
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Atoms/Button',
+    component: Button,
+    argTypes: {
+        children: {
+            control: { type: 'text' },
+        },
+        attributes: {
+            control: { type: 'object' },
+        }
+    },
+} as ComponentMeta<typeof Button>;
+
+const Template: Story<ButtonProps> = (args: any) => {
+
+    const { attributes, ...rest } = args;
+
+    return (
+        <Button {...attributes} {...rest}></Button>
+    );
+};
+
+export const Primary = Template.bind({});
+Primary.storyName = 'Button';
+Primary.args = {
+    children: 'button'
+}
