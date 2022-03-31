d36a84f7fe84a15fc69c660016aceaac95c2a291
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-03-31 17:13:40 +0100

    setup SideTab component logic (fix failing tests)

--- src/components/common/SideTab/SideTab.tsx
+++ src/components/common/SideTab/SideTab.tsx
@@ -1,8 +1,46 @@
-export type SideTabProps = {}
+import { useState } from 'react';
+import Button from '@components/common/Button/Button'
 
-const SideTab = (props: SideTabProps) => {
+export type SideTabProps = {
+    label: string
+    children: any
+}
+
+const SideTab = ({ label, children }: SideTabProps) => {
+
+    if (!children) return (<></>);
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const renderButton = () => {
+
+        return (
+            <Button
+                className='site-tab__button'
+                aria-expanded={isOpen}
+                aria-haspopup='true'
+                onClick={() => handleButtonClick()}
+            >{label}</Button>
+        )
+    }
+
+    const renderContent = () => {
+        return (
+            <div className='side-tab__content' aria-hidden={!isOpen} role='dialoge' >
+                {children}
+            </div>
+        )
+    }
+
     return (
-        <div className="site-tab"></div>
+        <aside className='site-tab' role='complementary'>
+            {renderButton()}
+            {renderContent()}
+        </aside>
     )
 }
 
