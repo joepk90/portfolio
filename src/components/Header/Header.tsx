d7fb26c2e84fe27ef318da5e313805a44fd9fbdf
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-01 14:06:40 +0100

    add homepage link to the header site title and site logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
 import { useState, ReactElement } from 'react';
 import Navigation from '@src/components/common/Navigation/Navigation'
 import DropdownNavigation from '@src/components/common/DropdownNavigation/DropdownNavigation'
 import Hamburger from '@components/common/Hamburger/Hamburger';
 import Seperator from '@components/common/Seperator/Seperator';
 import SiteBranding from '@src/components/SiteBranding/SiteBranding';
-import { menuItems } from '@src/config';
+import { menuItems, indexURL } from '@src/config';
 import '@components/Header/Header.scss'
+
 
 
 const Header = () => {
@@ -23,7 +25,12 @@ const Header = () => {
                 <div className="header__section">
                     <div className="header__details">
                         <div className="header__branding">
-                            <SiteBranding />
+                            <Link href={indexURL}>
+                                <a title="Home">
+                                    <SiteBranding />
+                                </a>
+                            </Link>
+
                         </div>
                         <div className="header__navigation-desktop">
                             <Navigation menuItems={menuItems} />
