662ba03b07b54256634e0a2bc4539348b9364342
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-09 17:07:24 +0100

    setup environment variable functions in environmentVars.ts utility file. replace environment variable usages in the application + setup SITE_URL environment variable in the next.config.js making it accessible during whole application lifecycle

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const withConfig = withGlobalCss()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  }
 }
-
 module.exports = withConfig(nextConfig)
