6f93961fad06cd92953b2c8cd1456d628a902b30
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-27 21:51:09 +0100

    add image height hack to ContentfulImage component - fix nextjs issue with image div being taller than the image

--- src/components/common/ContentfulImage/ContentfulImage.js
+++ src/components/common/ContentfulImage/ContentfulImage.js
@@ -1,14 +1,14 @@
 import Image from 'next/image';
+import '@src/components/common/ContentfulImage/ContentfulImage.scss';
 
 // TODO setup <picture><source> setup
-
 export const ContentfulImageAlias = ({ image, width, height }) => (
-  <picture>
+  <div className="contentful-image">
     <Image
       src={'https:' + image.fields.file.url}
       height={height}
       width={width}
       alt={image.fields.title}
     />
-  </picture>
+  </div>
 );
