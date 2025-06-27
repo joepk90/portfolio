b027f0c91db30a34c62cbb5eda2ad58ef96f3b5f
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-27 12:15:15 +0100

    update projects page to use SectionWithSeperator component

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,34 +2,38 @@ import type { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import { GetStaticProps } from 'next';
 import { getProjectsStaticProps, ProjectsProps } from '@src/lib/hooks/projects';
-import { Section } from '@components/common';
 import Container from '@src/components/common/Container/Container';
 import { Layout } from '@components/Layout/Layout';
 import Project from '@src/components/ProjectCard/ProjectCard';
+import { SectionWithSeperator, SectionWithSeperatorVariant } from '@components/common';
 import { getPageUrl } from '@lib/utilities/utilities';
 import { SEO, projectsURL } from '@src/config';
 
-import { BackgroundVariant } from '@src/lib/utilities/colors';
-
-const getBackgroundVariant = (key: number) =>
-  key % 2 === 0 ? BackgroundVariant.LightGrey : BackgroundVariant.DarkGrey;
+const getSectionVariant = (key: number) =>
+  key % 2 === 0 ? SectionWithSeperatorVariant.Light : SectionWithSeperatorVariant.Dark;
 
 const pageUrl = getPageUrl(projectsURL);
 
 export const getStaticProps: GetStaticProps = async () => getProjectsStaticProps();
 
 const Index: NextPage<ProjectsProps> = ({ projects }) => {
+  const projectsArrayLength = projects.length - 1;
   return (
     <Layout>
       <NextSeo title={`${SEO.title} | Projects`} canonical={pageUrl} />
 
       {projects.map((project, key) => {
         return (
-          <Section key={key} padding="lg" backgroundVariant={getBackgroundVariant(key)}>
+          <SectionWithSeperator
+            key={key}
+            padding="lg"
+            variant={getSectionVariant(key)}
+            disableDivider={key === projectsArrayLength}
+          >
             <Container>
               <Project key={key} project={project} />
             </Container>
-          </Section>
+          </SectionWithSeperator>
         );
       })}
     </Layout>
