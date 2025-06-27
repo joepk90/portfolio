59b22216e32aa7160a76bc12d45b132e7814bb31
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-27 13:01:03 +0100

    format the ProcessEnv interface in the additional typings file

--- additional.d.ts
+++ additional.d.ts
@@ -1,10 +1,10 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
-        CONTENTFUL_SPACE_ID: string
-        CONTENTFUL_ACCESS_TOKEN: string
-        NEXT_PUBLIC_SITE_URL: string
-        NEXT_PUBLIC_CHROMATIC_PROJECT_URL: string
-        NEXT_PUBLIC_GITHUB_URL: string
-        GOOGLE_TAG_MANAGER_ID: string
-    }
+  interface ProcessEnv {
+    CONTENTFUL_SPACE_ID: string;
+    CONTENTFUL_ACCESS_TOKEN: string;
+    NEXT_PUBLIC_SITE_URL: string;
+    NEXT_PUBLIC_CHROMATIC_PROJECT_URL: string;
+    NEXT_PUBLIC_GITHUB_URL: string;
+    GOOGLE_TAG_MANAGER_ID: string;
+  }
 }
