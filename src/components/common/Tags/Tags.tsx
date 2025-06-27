9f02d940f175a6da8acf09272e398b7826cb0bc0
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-27 13:32:57 +0100

    setup align variant option in the Tags component + update Tags export to export as module

--- src/components/common/Tags/Tags.tsx
+++ src/components/common/Tags/Tags.tsx
@@ -1,21 +1,36 @@
 import Tag, { TagProps } from '@components/common/Tag/Tag';
 import '@components/common/Tags/Tags.scss';
+import { generateBEMModifiersClassList } from '@src/lib/utilities';
+
+export enum TagsAlignmentVariant {
+  Left = 'left',
+  Right = 'right',
+}
+
+export const TagsAlignmentMap = {
+  [TagsAlignmentVariant.Left]: 'left',
+  [TagsAlignmentVariant.Right]: 'right',
+};
 
 export type TagsProps = {
   tags: string[];
+  align?: TagsAlignmentVariant;
   style?: React.CSSProperties;
 };
 
-const Tags = ({ tags, style }: TagsProps) => {
-  const renderTags = () => {
-    return tags.map((label, key) => {
-      return <Tag key={key} label={label} />;
-    });
-  };
+const renderTags = (tags: string[]) => {
+  return tags.map((label, key) => {
+    return <Tag key={key} label={label} />;
+  });
+};
+
+export const Tags = ({ tags, style, align = TagsAlignmentVariant.Left }: TagsProps) => {
+  const alignModifier = `align-${align}`;
+  const className = generateBEMModifiersClassList('tags', [alignModifier]);
 
   return (
-    <ul role="list" className="tags" style={{ ...style }}>
-      {renderTags()}
+    <ul role="list" className={className} style={{ ...style }}>
+      {renderTags(tags)}
     </ul>
   );
 };
