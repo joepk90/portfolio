d3dd17d4e983de0d4ce52195556b5833702d6e55
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-27 13:04:20 +0100

    fix google tag manager script setup - make env var public - prefix variable with NEXT_PUBLIC_

--- additional.d.ts
+++ additional.d.ts
@@ -5,6 +5,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_SITE_URL: string;
     NEXT_PUBLIC_CHROMATIC_PROJECT_URL: string;
     NEXT_PUBLIC_GITHUB_URL: string;
-    GOOGLE_TAG_MANAGER_ID: string;
+    NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: string;
   }
 }
