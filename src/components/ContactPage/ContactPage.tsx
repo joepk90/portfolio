07448048f4130dd4b2f1907c9a4abc4073c26c25
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-06-30 20:08:26 +0100

    setup ContactPage component shell in the components directory

---
+++ src/components/ContactPage/ContactPage.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+import { ContactProps } from '@src/lib/hooks/contact';
+import '@components/ContactPage/ContactPage.scss';
+
+export const ContactPage: FC<ContactProps> = ({
+  location,
+  email,
+  githubLink,
+  linkedInLink,
+  cvLink,
+}) => {
+  return <></>;
+};
