070d3b011f93a7740ac2246fa2f8cd989d926294
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-07-14 19:52:37 +0100

    refactor SideBarModal component to reuse the generic Portal component - add a title option + update styles

--- src/components/common/SideBarModal/SideBarModal.tsx
+++ src/components/common/SideBarModal/SideBarModal.tsx
@@ -1,29 +1,29 @@
 import { FC, ReactNode } from 'react';
-import { createPortal } from 'react-dom';
-import { useIsClient } from '@src/hooks';
-import { Button } from '@src/components/common/Button/Button';
+import { Button, Typography, Portal } from '@src/components/common';
 import '@components/common/SideBarModal/SideBarModal.scss';
 
 interface SideBarModalProps {
   isOpen: boolean;
   onClose: () => void;
   children: ReactNode;
+  title?: string;
 }
 
-export const SideBarModal: FC<SideBarModalProps> = ({ isOpen, onClose, children }) => {
-  const isClient = useIsClient();
+export const SideBarModal: FC<SideBarModalProps> = ({ isOpen, onClose, title, children }) => {
+  if (!isOpen) return null;
 
-  if (!isClient || !isOpen) return null;
+  return (
+    <Portal>
+      <div className="side-bar-modal__window">
+        <div className="side-bar-modal__header">
+          {title && <Typography variant="heading2">{title}</Typography>}
+          <Button onClick={onClose} className="side-bar-modal__close-button">
+            X
+          </Button>
+        </div>
 
-  return createPortal(
-    <div className="side-bar-modal__overlay">
-      <div className="side-bar-modal__header">
-        <Button onClick={onClose} className="side-bar-modal__close-button">
-          X
-        </Button>
+        <div className="side-bar-modal__content">{children}</div>
       </div>
-      <div className="side-bar-modal__content">{children}</div>
-    </div>,
-    document.body, // or a specific #modal-root div
+    </Portal>
   );
 };
