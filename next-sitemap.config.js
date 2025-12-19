7d51595bcabaedf64acf9481ab2aa768371b1d15
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-12-19 14:55:59 +0000

    fix next-sitemap.config.js file - use ESM syntax

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 
-module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost/3000',
-}
+const config = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com',
+  generateRobotsTxt: true,
+};
+
+export default config;
