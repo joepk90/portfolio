721c09f1dca0519e646d137220672248c1f40ed9
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-12-19 14:26:15 +0000

    convert ContentfulImage to tsx

---
+++ src/components/common/ContentfulImage/ContentfulImage.tsx
@@ -0,0 +1,32 @@
+import '@src/components/common/ContentfulImage/ContentfulImage.scss';
+
+import Image from 'next/image';
+
+type ContentfulImageProps = {
+  fields: {
+    title: string;
+    file: {
+      url: string;
+    };
+  };
+};
+
+type ContentfulImageAliasProps = {
+  image: ContentfulImageProps;
+  width: number;
+  height: number;
+};
+
+// TODO setup <picture><source> setup
+export const ContentfulImageAlias = ({ image, width, height }: ContentfulImageAliasProps) => {
+  return (
+    <div className="contentful-image">
+      <Image
+        src={'https:' + image.fields.file.url}
+        height={height}
+        width={width}
+        alt={image.fields.title}
+      />
+    </div>
+  );
+};
